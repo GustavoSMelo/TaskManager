584887a71aceade36ac0ffee1edb75462773543e
GustavoSMelo <gsantos15569@gmail.com>
2021-01-27 23:56:21 -0300

    creating a controller of tasks

--- Backend/app/Http/Controllers/TaskController.php
+++ Backend/app/Http/Controllers/TaskController.php
@@ -2,20 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TaskModel;
+use App\Models\UserModel;
+use Exception;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index(){
-
+    public function index(Request $request) {
+        try {
+            return UserModel::find($request->user()->currentAccessToken->tokenable->id);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'this user dont have any task registred'
+            ]);
+        }
     }
 
-    public function store () {
+    public function store (Request $request) {
 
+        $days_tasks = TaskModel::where('week_days', $request->input('week_days'));
+
+        foreach ($days_tasks as $task) {
+            if($request->input('week_days') >= $task->start_at && $request->input('week_days') <= $task->end_at) {
+                return response()->json([
+                    'error' => 'you already have a task in this period'
+                ], 400);
+            }
+        }
+
+        $newTask = new TaskModel();
+
+        $newTask->name = $request->input('name');
+        $newTask->description = $request->input('description');
+        $newTask->user_id = $request->user()->currentAccessToken()->tokenable->id;
+        $newTask->start_at = $request->input('start_at');
+        $newTask->end_at = $request->input('end_at');
+        $newTask->week_days = $request->input('week_days');
+
+        $newTask->save();
+
+        return response()->json([
+            'message' => 'task created with success'
+        ]);
     }
 
-    public function show(){
+    public function show (int $id){
 
+        return TaskModel::where('id', $id)->first();
+        
     }
 
     public function update(){
