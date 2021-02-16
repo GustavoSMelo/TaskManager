5ef7df0be3d9e9e1cf12d5c485286b5a174d9d00
GustavoSMelo <gsantos15569@gmail.com>
2021-02-15 23:55:28 -0300

    style of home page done

--- frontend/styles/home.style.ts
+++ frontend/styles/home.style.ts
@@ -1,67 +1,113 @@
-import styled, {keyframes} from 'styled-components'
-
-const borderAnimation = keyframes`
-    from {
-        transform: translateY(50%)
-    } to {
-        transform: translateY(45%)
-    }
-`
+import styled from 'styled-components'
 
 export const Container = styled.main`
 
-    display: flex;
     background-color: #C1C7E3;
-    height: 100vh;
+    display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    height: 100vh;
 
     h1 {
+        font-size: 26pt;
         font-family: Verdana, Geneva, Tahoma, sans-serif;
         color: #1d2c4f;
+        text-align: center;
+        margin: 30px;
     }
 
-    ul {
-        margin-top: 40px;
+    article {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        width: 100%;
+        height: 100%;
+        grid-gap: 10px;
+        padding: 30px;
     }
 
-    ul > li {
-        margin: 10px;
-        list-style-type: none;
-        font-size: 16pt;
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-        padding: 20px;
-        transition: 0.5s;
+    section {
+        display: flex;
+        align-items: center;
+        justify-content: center;
         cursor: pointer;
-    }
-
-    ul > li:hover {
-        font-size: 18pt;
-        transition: 0.5s;
+        border: solid 3px #1d2c4f;
         border-radius: 5px;
-    }
-
-    ul > li:nth-child(2) {
-        font-size: 22pt;
         transition: 0.5s;
-        border-top: 3px solid #1d2c4f;
-        border-bottom: 3px solid #1d2c4f;
-        border-left: 3px solid #1d2c4f;
-        border-right: 3px solid #1d2c4f;
-        border-radius: 5px;
     }
 
-    button {
+    section > h1 {
+        color: #1d2c4f;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-weight: bold;
+    }
+
+    section:hover {
+        box-shadow: 1px 1px 6px #303030;
+        transition: 0.5s;
+    }
+
+    .choosedDay {
         background-color: #1d2c4f;
         border: none;
-        border-radius: 5px;
-        padding: 20px;
-        margin: 15px;
-        color: #fff;
-        font-size: 16pt;
-        cursor: pointer;
+        transition: 0.5s;
+        cursor: default;
+    }
 
+    .choosedDay > h1 {
+        color: #fff !important;
+    }
+
+    .btnNotChoosed {
+        width: 300px;
+        margin: 30px auto;
+        cursor: not-allowed;
+        background-color: transparent;
+        border: 3px solid #1d2c4f;
+        padding: 10px;
+        font-size: 16pt;
+        font-weight: bold;
+        color: #1d2c4f;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 5px;
+        transition: 0.5s;
+    }
+
+    .btnChoosed {
+        width: 300px;
+        margin: 30px auto;
+        cursor: pointer;
+        background-color: #1d2c4f;
+        border: 3px solid transparent;
+        padding: 10px;
+        font-size: 16pt;
+        font-weight: bold;
+        color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 5px;
+        transition: 0.5s;
+    }
+
+    @media screen and (max-width: 920px){
+
+        article {
+            grid-template-columns: 1fr 1fr;
+        }
+    }
+
+    @media screen and (max-width: 660px) {
+
+        height: 100%;
+
+        article {
+            grid-template-columns: 1fr;
+        }
+
+        section {
+            padding: 30px;
+        }
     }
 
 `
