35b860ec6252f7f7214e716839b869a5c72d1009
GustavoSMelo <gsantos15569@gmail.com>
2021-02-22 23:56:18 -0300

    still working in day page

--- frontend/styles/task.day.style.ts
+++ frontend/styles/task.day.style.ts
@@ -7,13 +7,56 @@ export const Container = styled.main`
     justify-content: center;
 `
 
-export const WithoutContainer = styled.section`
+export const WithoutContainer = styled.section<{formDisplayed: string}>`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     width: 100vw;
     margin-top: 5%;
+
+    .CloseIcon {
+        font-size: 32pt;
+        cursor: pointer;
+        color: #fff;
+    }
+
+    form {
+        display: ${props => props.formDisplayed};
+        flex-direction: column;
+        background-color: #101010dd;
+        position: absolute;
+        width: 100vw;
+        height: 90vh;
+        align-items: center;
+        justify-content: center;
+    }
+
+    form > h1 {
+        font-size: 22pt;
+        font-weight: bold;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        color: #fff;
+    }
+
+    form > input, form > textarea, form > select {
+        width: 30%;
+        margin: 10px;
+        border: none;
+        border-radius: 5px;
+        padding: 15px;
+        font-size: 14pt;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+
+    form > textarea {
+        resize: none;
+        height: 200px;
+    }
+
+    form > select {
+        cursor: pointer;
+    }
 
     p {
         color: #1d2c4f;
