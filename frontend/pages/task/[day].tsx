504b2d205c87d34483ae30ecc255448acb8abb54
GustavoSMelo <gsantos15569@gmail.com>
2021-02-22 23:56:09 -0300

    still working in day page

--- frontend/pages/task/[day].tsx
+++ frontend/pages/task/[day].tsx
@@ -1,9 +1,11 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import { Container, WithoutContainer } from '../../styles/task.day.style'
 import DayInterface from '../../interface/NavbarInterface'
 import api from '../../api/api'
 import Image from 'next/image'
+import { FaWindowClose } from 'react-icons/fa'
+import Popup from '../../components/PopupBox'
 
 export async function getServerSideProps(ctx: any) {
     const day = ctx.query.day
@@ -24,6 +26,8 @@ const Task : React.FC<DayInterface> = (props) => {
     const [week_days, setWeek_days] = useState('')
     const [start_at, setStart_at] = useState('')
     const [end_at, setEnd_at] = useState('')
+    const [openForm, setOpenForm] = useState('none')
+    const [PopupBox, setPopupBox] = useState(<></>)
 
     const getDataByAPI = async () => {
         try {
@@ -34,8 +38,12 @@ const Task : React.FC<DayInterface> = (props) => {
                 }
             })
 
-            //setTasks(response.data)
+            setTasks(response.data)
 
+
+            setTimeout(() => {
+                setPopupBox(<></>)
+            }, 3000)
             console.log({resposta: response})
         } catch (err) {
             console.log({Error: err})
@@ -45,23 +53,57 @@ const Task : React.FC<DayInterface> = (props) => {
 
     useEffect(() => {
         getDataByAPI()
-    }, [])
+    }, [PopupBox])
+
+    const handlerCreateTask = async () => {
+
+        try {
+            await api.post('/api/task', {
+                name, description, week_days, start_at, end_at
+            }, {headers: {
+                Authorization: `Bearer ${localStorage.getItem('bearer-token')}`,
+            }})
+
+            setPopupBox(<Popup textColor='#0AFA00' backgroundColor='#CEFAD5' message='Task created with success ' />)
+        } catch (err) {
+            setPopupBox(<Popup textColor='#f00' backgroundColor='#FCD9D5' message='Error to create a task, please try again' />)
+
+            console.error({Error: err})
+        }
+    }
 
     const viewWithoutTask = () => {
         return (
-            <WithoutContainer>
+            <WithoutContainer formDisplayed={openForm}>
                 <Image src='/images/void.svg' width={600} height={500}/>
                 <p>
                     Hey!<br />
                     look's like you don't have any task registred yet
                 </p>
-                <button>Create a new task</button>
+                <button onClick={() => setOpenForm('flex')}>Create a new task</button>
 
                 <form>
+                    <FaWindowClose className="CloseIcon" onClick={() => setOpenForm('none')}/>
                     <h1>Create your task here</h1>
-                    <input placeholder='insert a task name here... ' name='name' id='name' value={name} required/>
-                    <textarea placeholder='insert a description of task here' name='description' id='description' value={description} required/>
-                    <select value={week_days}>
+
+                    <input placeholder='insert a task name here... '
+                        type='text'
+                        name='name'
+                        id='name'
+                        value={name}
+                        onChange={event => setName(event.target.value)}
+                        required
+                    />
+
+                    <textarea
+                        placeholder='insert a description of task here'    name='description'
+                        id='description'
+                        value={description}
+                        onChange={event => setDescription(event.target.value)}
+                        required
+                    />
+
+                    <select value={week_days} onChange={e => setWeek_days(e.target.value)}>
                         <option value={''}>Choose you day</option>
                         <option value='Sunday'>Sunday</option>
                         <option value='Monday'>Monday</option>
@@ -71,6 +113,28 @@ const Task : React.FC<DayInterface> = (props) => {
                         <option value='Friday'>Friday</option>
                         <option value='Saturday'>Saturday</option>
                     </select>
+
+                    <input
+                        placeholder='Start at..'
+                        name='name'
+                        type='number'
+                        id='name'
+                        value={start_at}
+                        onChange={event => setStart_at(event.target.value)}
+                        required
+                    />
+
+                    <input
+                        placeholder='End at.. '
+                        name='name'
+                        type='number'
+                        id='name'
+                        value={end_at}
+                        onChange={event => setEnd_at(event.target.value)}
+                        required
+                    />
+
+                    <button type='button' onClick={() => handlerCreateTask()}> Create Task</button>
                 </form>
             </WithoutContainer>
         )
@@ -86,6 +150,7 @@ const Task : React.FC<DayInterface> = (props) => {
         <Container>
             <Navbar day={props.day} />
             {tasks.length <= 0 ? viewWithoutTask() : showTheTasks()}
+            {PopupBox}
         </Container>
     )
 
